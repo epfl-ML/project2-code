dd0bcff7417b5c0ca4006939d12cea2de642917c
Lars Barmettler <lars.barmettler@ost.ch>
2022-12-01 08:46:10 +0100

    refactor Epoch class

--- data-extraction/script.py
+++ data-extraction/script.py
@@ -5,15 +5,24 @@ import os
 
 class Epoch:
 	def __init__(self, bytes):
-		state = chr(bytes[0])
-		EEGv = struct.unpack('f', bytes[-12:-8])[0]
-		EMGv = struct.unpack('f', bytes[-8:-4])[0]
-		temp = struct.unpack('f', chunk[-4:])[0]
+		self.state = self.state_maping(chr(bytes[0]))
+		self.EEGv = struct.unpack('f', bytes[-12:-8])[0]
+		self.EMGv = struct.unpack('f', bytes[-8:-4])[0]
+		self.temp = struct.unpack('f', chunk[-4:])[0]
 
 	def write_row(self, thewriter):
-		thewriter.writerow([chr(chunk[0]), struct.unpack('f', chunk[-12:-8])[0], struct.unpack('f', chunk[-8:-4])[0], struct.unpack('f', chunk[-4:])[0]])
+		thewriter.writerow([self.state, self.EEGv, self.EMGv, self.temp])
 
-
+	def state_maping(self, state):
+		if state == 'w' or '1' or '4':
+			return 3
+		if state == 'n' or '2' or '5':
+			return 2
+		if state == 'r' or '3' or '6':
+			return 1
+		else: 
+			return 0
+		
 chunk_size = 1617 # 1 state byte + 401 DC-component of the EEG + EEG variability, the EMG variability and the temperature of the mouse
 
 dirname = os.path.dirname(__file__)
