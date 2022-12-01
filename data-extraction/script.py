13699dd66ffcf140692a8e10089d01ee05de5cba
Lars Barmettler <lars.barmettler@ost.ch>
2022-12-01 08:09:41 +0100

    fix an other stupid bug

--- data-extraction/script.py
+++ data-extraction/script.py
@@ -1,6 +1,7 @@
 import codecs
 import struct
 import csv
+import os
 
 class Epoch:
 	def __init__(self, bytes):
@@ -9,16 +10,18 @@ class Epoch:
 		EMGv = struct.unpack('f', bytes[-8:-4])[0]
 		temp = struct.unpack('f', chunk[-4:])[0]
 
-	def write_row(thewriter):
+	def write_row(self, thewriter):
 		thewriter.writerow([chr(chunk[0]), struct.unpack('f', chunk[-12:-8])[0], struct.unpack('f', chunk[-8:-4])[0], struct.unpack('f', chunk[-4:])[0]])
 
 
 chunk_size = 1617 # 1 state byte + 401 DC-component of the EEG + EEG variability, the EMG variability and the temperature of the mouse
 
+dirname = os.path.dirname(__file__)
+
 output_file = '../data/10101.csv'
 input_file = '../data/10101.smo'
 
-with open(output_file, 'w', newline='') as csvfile:
+with open(os.path.join(dirname, output_file), 'w', newline='') as csvfile:
 	thewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	thewriter.writerow(["state", "EEGv", "EMGv", "temp"])
 	with open(input_file, "rb") as in_file:
